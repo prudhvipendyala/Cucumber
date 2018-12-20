f64e0664ec638e45566c025964fa536114574e6b
prudhvi.pendyala <prudhvi.pendyala@leoforce.local>
2018-12-20 14:22:11 +0530

    test

---
+++ src/main/java/jenkinsproject/jenkins/App.java
@@ -0,0 +1,17 @@
+package jenkinsproject.jenkins;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	@Test
+    public static void main()
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("This is my first project to execute from jenkins");
+    }
+}
