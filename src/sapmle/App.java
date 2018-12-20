22e01d06527d756c6d0475d209ca71dcf551c589
prudhvi.pendyala <prudhvi.pendyala@leoforce.local>
2018-12-20 15:27:46 +0530

    sample project

---
+++ src/sapmle/App.java
@@ -0,0 +1,16 @@
+package sapmle;
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	@Test
+    public static void main()
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("This is my first project to execute from jenkins");
+    }
+}
